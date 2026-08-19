0a46046a4d451bd59d2e6d9630ff7524c6c82082
100k1-game <kaunkirill15@gmail.com>
2026-08-19 12:23:19 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,7 +7,25 @@ createApp({
         const error = ref('');
 
         const formatInput = () => {
-            keyInput.value = keyInput.value.toUpperCase().replace(/[^A-Z0-9-]/g, '');
+            error.value = '';
+            let val = keyInput.value.toUpperCase().replace(/[^A-Z0-9]/g, '');
+            if (val.startsWith('100K')) {
+                val = val.substring(4);
+            }
+            if (val.length === 0) {
+                keyInput.value = '';
+                return;
+            }
+            // Форматируем как 100K-XXXX-XXXX
+            let formatted = '100K';
+            if (val.length > 0) formatted += '-' + val.substring(0, 4);
+            if (val.length > 4) formatted += '-' + val.substring(4, 8);
+            
+            keyInput.value = formatted;
+
+            if (formatted.length === 14 && !keyValidated.value) {
+                validateKey();
+            }
         };
 
         const validateKey = () => {
